5caf73049cb3ee3ff23e5bcc3fa768e0c28d0a64
Ryan Iguchi <ryan.iguchi1@gmail.com>
2022-12-24 19:50:05 +0100

    added comonent for sign-in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/routes/home/home.component';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in-component';
+
+const Shop = () => {
+  return <h1>this is the Shop</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
   );
 };
