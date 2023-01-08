06a7ddc9fadac0601dcd3567a8fd19856f2d94ae
Ryan Iguchi <ryan.iguchi1@gmail.com>
2023-01-08 13:38:55 +0100

    added redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,18 @@
 import { createStore, applyMiddleware, compose } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import logger from 'redux-logger';
 import { rootReducer } from './root.reducer';
+import { Action } from '@remix-run/router';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
   Boolean
@@ -9,4 +20,10 @@ const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
